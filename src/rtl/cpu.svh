dbfa049255ad664d8a97ce670288a2f4b052298e
Scrawach <scrawach@gmail.com>
2022-05-20 14:34:34 +0300

    added isa defines for arithmetic instructions

--- src/rtl/cpu.svh
+++ src/rtl/cpu.svh
@@ -11,6 +11,7 @@
 `define ALUOP_SLTU    3'b100
 `define ALUOP_SUB     3'b101
 `define ALUOP_SRC1    3'b111
+`define ALUOP_SRA     3'b110
 
 // MUXes 
 `define ALUSRC1_RS2  2'b00
@@ -34,7 +35,10 @@
 `define BRANCH_GREATER_SIGN 3'b101
 
 // instruction opcode
-`define RVOP_ADDI   7'b0010011
+`define RVOP_LUI    7'b0110111
+`define RVOP_AUIPC  7'b0010111
+`define RVOP_JAL    7'b1101111
+`define RVOP_JALR   7'b1100111
 
 `define RVOP_BEQ    7'b1100011
 `define RVOP_BNE    7'b1100011
@@ -43,21 +47,36 @@
 `define RVOP_BLTU   7'b1100011
 `define RVOP_BGEU   7'b1100011
 
-`define RVOP_LUI    7'b0110111
-`define RVOP_ADD    7'b0110011
-`define RVOP_OR     7'b0110011
-`define RVOP_SRL    7'b0110011
-`define RVOP_SLL    7'b0110011
-`define RVOP_SLTU   7'b0110011
-`define RVOP_SUB    7'b0110011
 `define RVOP_LW     7'b0000011
 `define RVOP_SW     7'b0100011
-`define RVOP_JAL    7'b1101111
-`define RVOP_JALR   7'b1100111
-`define RVOP_AUIPC  7'b0010111
+
+`define RVOP_ADDI   7'b0010011
+`define RVOP_SLTI   7'b0010011
+`define RVOP SLTIU  7'b0010011
+`define RVOP_XORI   7'b0010011
+`define RVOP_ORI    7'b0010011
+`define RVOP_ANDI   7'b0010011
+`define RVOP_SLLI   7'b0010011
+`define RVOP_SRLI   7'b0010011
+`define RVOP_SRAI   7'b0010011
+
+`define RVOP_ADD    7'b0110011
+`define RVOP_SUB    7'b0110011
+`define RVOP_SLL    7'b0110011
+`define RVOP_SLT    7'b0110011
+`define RVOP_SLTU   7'b0110011
+`define RVOP_XOR    7'b0110011
+`define RVOP_SRL    7'b0110011
+`define RVOP_SRA    7'b0110011
+`define RVOP_OR     7'b0110011
+`define RVOP_AND    7'b0110011
 
 // instruction funct3
-`define RVF3_ADDI   3'b000
+`define RVF3_ANY    3'b???
+`define RVF3_JALR   3'b000
+
+`define RVF3_LW     3'b010
+`define RVF3_SW     3'b010
 
 `define RVF3_BEQ    3'b000
 `define RVF3_BNE    3'b001
@@ -66,25 +85,43 @@
 `define RVF3_BLTU   3'b110
 `define RVF3_BGEU   3'b111
 
-`define RVF3_ADD    3'b000
-`define RVF3_OR     3'b110
-`define RVF3_SRL    3'b101
-`define RVF3_SLL    3'b001
-`define RVF3_SLTU   3'b011
-`define RVF3_SUB    3'b000
-`define RVF3_LW     3'b010
-`define RVF3_SW     3'b010
-`define RVF3_JALR   3'b000
+`define RVF3_ADDI   3'b000
+`define RVF3_SLTI   3'b010
+`define RVF3 SLTIU  3'b011
+`define RVF3_XORI   3'b100
+`define RVF3_ORI    3'b110
+`define RVF3_ANDI   3'b111
+`define RVF3_SLLI   3'b001
+`define RVF3_SRLI   3'b101
+`define RVF3_SRAI   3'b101
 
-`define RVF3_ANY    3'b???
+`define RVF3_ADD    3'b000
+`define RVF3_SUB    3'b000
+`define RVF3_SLL    3'b001
+`define RVF3_SLT    3'b010
+`define RVF3_SLTU   3'b011
+`define RVF3_XOR    3'b100
+`define RVF3_SRL    3'b101
+`define RVF3_SRA    3'b101
+`define RVF3_OR     3'b110
+`define RVF3_AND    3'b111
 
 // instruction funct7
-`define RVF7_ADD    7'b0000000
-`define RVF7_OR     7'b0000000
-`define RVF7_SRL    7'b0000000
-`define RVF7_SLL    7'b0000000
-`define RVF7_SLTU   7'b0000000
-`define RVF7_SUB    7'b0100000
 `define RVF7_ANY    7'b???????
+
+`define RVF7_SLLI   7'b0000000
+`define RVF7_SRLI   7'b0000000
+`define RVF7_SRAI   7'b0100000
+
+`define RVF7_ADD    7'b0000000
+`define RVF7_SUB    7'b0100000
+`define RVF7_SLL    7'b0000000
+`define RVF7_SLT    7'b0000000
+`define RVF7_SLTU   7'b0000000
+`define RVF7_XOR    7'b0000000
+`define RVF7_SRL    7'b0000000
+`define RVF7_SRA    7'b0100000
+`define RVF7_OR     7'b0000000
+`define RVF7_AND    7'b0000000
 // ------------
 `endif
