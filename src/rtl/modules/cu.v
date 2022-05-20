81c31bcc2b3d8cb30daf67240b5f973845d6c816
Scrawach <scrawach@gmail.com>
2022-05-20 15:46:36 +0300

    implemented control operation for ALU in control unit (cu)

--- src/rtl/modules/cu.v
+++ src/rtl/modules/cu.v
@@ -10,7 +10,7 @@ module cu
  output       rd_write,
  output [1:0] rd_write_src,
  output       mem_write,
- output [2:0] alu_op,
+ output [3:0] alu_op,
  output [1:0] alu_src1,
  output       alu_src2,
  output       fast_jump,
@@ -20,7 +20,7 @@ module cu
 
   /*AUTOREG*/
   // Beginning of automatic regs (for this module's undeclared outputs)
-  reg [2:0]             alu_op;
+  reg [3:0]             alu_op;
   reg [1:0]             alu_src1;
   reg                   alu_src2;
   reg                   branch;
@@ -36,7 +36,7 @@ module cu
   always @ ( * ) begin
     pc_write         = 1'b0;
     rd_write         = 1'b0;
-    alu_op           = 3'b000;
+    alu_op           = `ALUOP_ADD;
     rd_write_src     = `RDSRC_ALU;
     mem_write        = 1'b0;
     branch           = 1'b0;
@@ -69,27 +69,27 @@ module cu
       { `RVF7_ANY,  `RVF3_SW,  `RVOP_SW } : begin alu_src1 = `ALUSRC1_IMMS; mem_write = 1'b1; end
 
       // I-Type:
-      { `RVF7_ANY,  `RVF3_ADDI,  `RVOP_ADDI  } : begin rd_write = 1'b1; alu_op = `ALUOP_ADD; alu_src1 = 2'b1; end
-      { `RVF7_ANY,  `RVF3_SLTI,  `RVOP_SLTI  } : begin end
-      { `RVF7_ANY,  `RVF3_SLTIU, `RVOP_SLTIU } : begin end
-      { `RVF7_ANY,  `RVF3_XORI,  `RVOP_XORI  } : begin end
-      { `RVF7_ANY,  `RVF3_ORI,   `RVOP_ORI   } : begin end
-      { `RVF7_ANY,  `RVF3_ANDI,  `RVOP_ANDI  } : begin end
-      { `RVF7_SLLI, `RVF3_SLLI,  `RVOP_SLLI  } : begin end
-      { `RVF7_SRLI, `RVF3_SRLI,  `RVOP_SRLI  } : begin end
-      { `RVF7_SRAI, `RVF3_SRAI,  `RVOP_SRAI  } : begin end
+      { `RVF7_ANY,  `RVF3_ADDI,  `RVOP_ADDI  } : begin rd_write = 1'b1; alu_op = `ALUOP_ADD;  alu_src1 = `ALUSRC1_IMMI; end
+      { `RVF7_ANY,  `RVF3_SLTI,  `RVOP_SLTI  } : begin rd_write = 1'b1; alu_op = `ALUOP_SLT;  alu_src1 = `ALUSRC1_IMMI; end
+      { `RVF7_ANY,  `RVF3_SLTIU, `RVOP_SLTIU } : begin rd_write = 1'b1; alu_op = `ALUOP_SLTU; alu_src1 = `ALUSRC1_IMMI; end
+      { `RVF7_ANY,  `RVF3_XORI,  `RVOP_XORI  } : begin rd_write = 1'b1; alu_op = `ALUOP_XOR;  alu_src1 = `ALUSRC1_IMMI; end
+      { `RVF7_ANY,  `RVF3_ORI,   `RVOP_ORI   } : begin rd_write = 1'b1; alu_op = `ALUOP_OR;   alu_src1 = `ALUSRC1_IMMI; end
+      { `RVF7_ANY,  `RVF3_ANDI,  `RVOP_ANDI  } : begin rd_write = 1'b1; alu_op = `ALUOP_AND;  alu_src1 = `ALUSRC1_IMMI; end
+      { `RVF7_SLLI, `RVF3_SLLI,  `RVOP_SLLI  } : begin rd_write = 1'b1; alu_op = `ALUOP_SLL;  alu_src1 = `ALUSRC1_IMMI; end
+      { `RVF7_SRLI, `RVF3_SRLI,  `RVOP_SRLI  } : begin rd_write = 1'b1; alu_op = `ALUOP_SRL;  alu_src1 = `ALUSRC1_IMMI; end
+      { `RVF7_SRAI, `RVF3_SRAI,  `RVOP_SRAI  } : begin rd_write = 1'b1; alu_op = `ALUOP_SRA;  alu_src1 = `ALUSRC1_IMMI; end
       
       // R-Type:
       { `RVF7_ADD,  `RVF3_ADD,   `RVOP_ADD   } : begin rd_write = 1'b1; alu_op = `ALUOP_ADD;  end
       { `RVF7_SUB,  `RVF3_SUB,   `RVOP_SUB   } : begin rd_write = 1'b1; alu_op = `ALUOP_SUB;  end
       { `RVF7_SLL,  `RVF3_SLL,   `RVOP_SLL   } : begin rd_write = 1'b1; alu_op = `ALUOP_SLL;  end
-      { `RVF7_SLT,  `RVF3_SLT,   `RVOP_SLT   } : begin end
+      { `RVF7_SLT,  `RVF3_SLT,   `RVOP_SLT   } : begin rd_write = 1'b1; alu_op = `ALUOP_SLT;  end
       { `RVF7_SLTU, `RVF3_SLTU,  `RVOP_SLTU  } : begin rd_write = 1'b1; alu_op = `ALUOP_SLTU; end
-      { `RVF7_XOR,  `RVF3_XOR,   `RVOP_XOR   } : begin end
+      { `RVF7_XOR,  `RVF3_XOR,   `RVOP_XOR   } : begin rd_write = 1'b1; alu_op = `ALUOP_XOR;  end
       { `RVF7_SRL,  `RVF3_SRL,   `RVOP_SRL   } : begin rd_write = 1'b1; alu_op = `ALUOP_SRL;  end
       { `RVF7_SRA,  `RVF3_SRA,   `RVOP_SRA   } : begin rd_write = 1'b1; alu_op = `ALUOP_SRA;  end
       { `RVF7_OR,   `RVF3_OR,    `RVOP_OR    } : begin rd_write = 1'b1; alu_op = `ALUOP_OR;   end
-      { `RVF7_AND,  `RVF3_AND,   `RVOP_AND   } : begin end
+      { `RVF7_AND,  `RVF3_AND,   `RVOP_AND   } : begin rd_write = 1'b1; alu_op = `ALUOP_AND;  end
     endcase // casez ( { func7, func3, cmd_op } )
   end
   
