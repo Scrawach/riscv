1fb9ab437b88e71cc616e49a5305bd34e9dc5526
Scrawach <scrawach@gmail.com>
2022-06-02 15:19:57 +0300

    bug fixed: using wire output address instead of register output

--- src/rtl/modules/csr.v
+++ src/rtl/modules/csr.v
@@ -18,29 +18,34 @@ module csr
   reg [31:0]            csr_data_out;
   // End of automatics
   reg [31:0]            wr_data;
+  reg [31:0]            csr_data_out_temp;
   reg [31:0]            debug;
   
   wire [31:0]           in_data;
 
-  assign in_data = csr_op[2] == `CSR_IMM ? { 27'b0, csr_uimm } : csr_data_in;       
+  assign in_data = csr_op[2] ? { 27'b0, csr_uimm } : csr_data_in;       
   
   always @ (*) begin
     case (csr_op[1:0])
-      `CSR_NOP : wr_data = csr_data_out;
+      `CSR_NOP : wr_data = csr_data_out_temp;
       `CSR_RW  : wr_data = in_data;
-      `CSR_RS  : wr_data = csr_data_out |  in_data;
-      `CSR_RC  : wr_data = csr_data_out & ~in_data;
+      `CSR_RS  : wr_data = csr_data_out_temp |  in_data;
+      `CSR_RC  : wr_data = csr_data_out_temp & ~in_data;
     endcase
+  end
+
+  always @ (*) begin
+    case (csr_addr)
+      12'd777  : csr_data_out_temp = debug;
+      default  : csr_data_out_temp = 32'h0;
+    endcase // case (csr_addr)
   end
 
   always @ ( posedge clk or negedge rst_n ) begin
     if ( !rst_n ) begin
       csr_data_out <= 32'h0;
     end else begin
-      case (csr_addr)
-        12'd777  : csr_data_out = debug;
-        default  : csr_data_out = 32'h0;
-      endcase // case (csr_addr)
+      csr_data_out <= csr_data_out_temp;
     end
   end
 
